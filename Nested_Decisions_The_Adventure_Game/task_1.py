b71eccb3b0bdd989215c3c0b87621cd2baf61e5d
codeland93 <erichfreeland@gmail.com>
2024-07-16 23:21:40 -0400

    updated code

--- Nested_Decisions_The_Adventure_Game/task_1.py
+++ Nested_Decisions_The_Adventure_Game/task_1.py
@@ -1,16 +1,24 @@
 place = input("Choose a place: forest or cave? ")
 
 if place == 'forest':
-    action = input("climb a tree or cross a river?")
+    print("You are in a forest.")
+    action = input("Do you want to climb a tree or cross a river? ")
     if action == "climb a tree":
-        pass
         print("You found a bird's nest!")
     elif action == "cross a river":
         print("You found a boat!")
+    else:
+        pass  
 elif place == 'cave':
-    action = input("Do you want to light a torch or proceed in the dark")
+    print("You are in a cave.")
+    action = input("Do you want to light a torch or proceed in the dark? ")
     if action == "light a torch":
         print("You found a chest!")
     elif action == "proceed in the dark":
-        pass
-        print("Opps you met a monster!") 
+        print("Oops, you met a monster!")
+    else:
+        pass  
+else:
+    print("Invalid option. Please choose either 'forest' or 'cave'.")
+    action = input("Do you want to light a torch or proceed in the dark? ")
+    pass 
